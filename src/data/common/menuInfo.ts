9f6f7936bc78244ce9e0ed5fe897e6d386da31fb
soodev20 <soodev20@gmail.com>
2022-11-02 21:50:54 +0900

    Feat: extract menu data

---
+++ src/data/common/menuInfo.ts
@@ -0,0 +1,6 @@
+export const MENU_LIST = [
+  { title: 'About', path: '/about' },
+  { title: 'Posts', path: '/posts' },
+  { title: 'Projects', path: 'projects' },
+  { title: 'Tags', path: 'tags' }
+]
