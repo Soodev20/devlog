f235c9dc8bcb3446dad684e1b74962446f6559c6
soodev20 <soodev20@gmail.com>
2022-11-02 22:22:42 +0900

    Chore: clarify css

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,27 +10,25 @@ export default function Menu () {
   }
 
   return (
-    <nav className="px-2 py-2.5">
-      <div className="container relative inline-flex flex-row justify-between items-center overflow-hidden mx-auto">
-        <div className="flex items-center">
-          <a href="/">
-            <span className="text-xl font-bold whitespace-nowrap hover:text-main-blue">SoojSooj's Devlog</span>
-          </a>
-        </div>
-        <ul className="flex flex-row relative z-1 transition-duration:3ms">
-        {MENU_LIST.map(info => {
-          return(
-          <Link
-            className={`px-4 hover:text-main-blue ${checkNowPath(info.path) && 'border-b-2 border-b-main-orange'}`}
-            key={info.title}
-            href={info.path}
-          >
-            <li>{info.title}</li>
-          </Link>
-          )
-        })}
-        </ul>
+    <nav className="px-8 py-4 container flex flex-row justify-between items-center mx-auto">
+      <div className="flex items-center">
+        <a href="/">
+          <span className="text-xl font-bold whitespace-nowrap hover:text-main-blue dark:text-white">SoojSooj's Devlog</span>
+        </a>
       </div>
+      <ul className="flex flex-row">
+      {MENU_LIST.map(info => {
+        return(
+        <Link
+          className={`px-4 hover:text-main-blue dark:text-white ${checkNowPath(info.path) && 'border-b-2 border-b-main-orange'}`}
+          key={info.title}
+          href={info.path}
+        >
+          <li>{info.title}</li>
+        </Link>
+        )
+      })}
+      </ul>
     </nav>
   );
 }
