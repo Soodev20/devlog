10ea164e3e05562bb5f1b85faa49f599eb220160
soodev20 <soodev20@gmail.com>
2022-11-03 23:51:00 +0900

    Mid-save: tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Introduce from '../src/components/Introduce'
+import Tag from '../src/components/tag'
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center m-10">
         <Introduce />
+        <div className='flex flex-wrap w-fit bg-red-300'>
+          <Tag />
+          <Tag />
+          <Tag />
+          <Tag />
+        </div>
       </div>
     </>
   )
---
+++ src/components/tag.tsx
@@ -0,0 +1,9 @@
+export default function Tag () {
+  return (
+    <div className='m-2 px-2 rounded-full border-main-gray border-2'>
+      <span>
+        javascript
+      </span>
+    </div>
+  )
+}
