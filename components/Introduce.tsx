8b4329b0b475ea70a100f4c15e8a30b8428f012a
soodev20 <soodev20@gmail.com>
2022-11-17 23:22:20 +0900

    Feat: Introduce

--- components/Introduce.tsx
+++ components/Introduce.tsx
@@ -2,10 +2,10 @@ import ProfileImage from './ProfileImage'
 
 export default function Introduce () {
   return (
-    <div className='flex flex-row w-fit mb-10'>
+    <div className='flex flex-row w-fit'>
       <ProfileImage />
-      <div className='flex flex-col justify-end items-start'>
-        <h1 className='text-lg'>Soojeong Choi</h1>
+      <div className='flex flex-col ml-4 sm:ml-8 justify-center items-start'>
+        <h1 className='text-xl'>Soojeong Choi</h1>
         <span className='text-base'>rkskekfdlkfjklsdj;fasjdfl;adsjkflkjwefql;</span>
       </div>
     </div>
