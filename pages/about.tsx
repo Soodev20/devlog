c5999a73489c3a23b70448db5c08ec5484d4b346
soodev20 <soodev20@gmail.com>
2022-11-17 23:27:36 +0900

    Chore: Change single quote

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="flex flex-col items-center justify-center m-10">
+    <div className='flex flex-col items-center justify-center m-10'>
       <p>This is about page.</p>
     </div>
   )
