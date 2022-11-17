fb7bb2bc80d475f9df743f5942312c6a0b69f814
soodev20 <soodev20@gmail.com>
2022-11-17 23:27:25 +0900

    Chore: Change single quote

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
-import Menu from "../components/Menu";
-import "../styles/globals.css";
+import Menu from '../components/Menu';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
