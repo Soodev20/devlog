f7e8a007cbd03b7df4e99d5371031cff614495d4
soodev20 <soodev20@gmail.com>
2022-11-17 23:25:20 +0900

    Feat: Tag

---
+++ components/Tags.tsx
@@ -0,0 +1,23 @@
+import { iPostsType } from '../type/type';
+import Tag from './Tag';
+
+type SortedTagInfo = Record<string, number>
+
+export default function Tags ({ posts }: iPostsType) {
+  const flatMappedTags = posts.flatMap(data => data.tag)
+  const sortedTags = {} as SortedTagInfo
+
+  for (let i = 0; i < flatMappedTags.length; i++) {
+    sortedTags[flatMappedTags[i]] ? sortedTags[flatMappedTags[i]] += 1 : sortedTags[flatMappedTags[i]] = 1
+  }
+
+  return (
+    <>
+      {Object.keys(sortedTags).map(key => {
+        return (
+          <Tag subject={key} frequency={sortedTags[key]}/>
+        )
+      })}
+    </>
+  )
+}
--- components/tag.tsx
+++ components/tag.tsx
@@ -1,11 +1,13 @@
 type TagProps = {
   subject: string;
+  frequency?: number;
 };
 
-export default function Tag ({ subject }: TagProps) {
+export default function Tag ({ subject, frequency }: TagProps) {
   return (
-    <div className='m-2 px-2 rounded-full border-main-gray border-2 text-main-gray'>
+    <div className='text-sm m-2 px-2 sm:text-sm1 rounded-full border-main-gray border-[0.1rem] sm:border-2 text-main-gray hover:shadow-xl cursor-pointer'>
       <span>{subject}</span>
+      {frequency && <span>{` (${frequency})`}</span>}
     </div>
   )
 }
