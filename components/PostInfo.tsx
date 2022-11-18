39e53e3bbdc6432f72f97a0a841f646ed55b77ce
soodev20 <soodev20@gmail.com>
2022-11-18 23:42:48 +0900

    Change: url to go post

--- components/PostInfo.tsx
+++ components/PostInfo.tsx
@@ -9,7 +9,7 @@ export default function PostInfo ({ post }: PostInfoProp) {
   const { title, description, date, tag, key } = post
 
   return (
-    <Link href={`/posts/${key}`} passHref>
+    <Link href={`/post/${key}`} passHref>
       <div className='flex flex-col container mb-4 sm:mb-6 p-4 rounded-lg border-main-gray border-2 text-main-gray hover:shadow-xl cursor-pointer'>
         <div className='flex flex-row container items-center'>
           <h2 className='text-main-blue font-bold text-xl'>{title}</h2>
