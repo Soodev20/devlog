c51a5dc438bf797eaeb7ff9ee943966aeac2cf9e
soodev20 <soodev20@gmail.com>
2022-11-19 02:23:09 +0900

    Chore: Add projects & tags page for prevent 404

---
+++ pages/projects.tsx
@@ -0,0 +1,9 @@
+const ProjectsPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center m-10'>
+      <p>This is projects page.</p>
+    </div>
+  )
+}
+
+export default ProjectsPage
---
+++ pages/tags.tsx
@@ -0,0 +1,9 @@
+const TagsPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center m-10'>
+      <p>This is tags page.</p>
+    </div>
+  )
+}
+
+export default TagsPage
