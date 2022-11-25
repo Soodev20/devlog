abe5cdd1fdc275c1e721a52e21c387cc558d4413
soodev20 <soodev20@gmail.com>
2022-11-25 23:26:56 +0900

    Chore: Move tags part to tags page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,17 @@
 import { GetStaticProps } from 'next'
 import Introduce from '../components/Introduce'
-import Tags from '../components/Tags'
 import { getAllPostsMetaData } from './api/getStaticData'
 import { iPostsType } from '../type/type'
 import PostInfos from '../components/PostInfos'
 
 export const IndexPage = ({ posts }: iPostsType): JSX.Element => {
   return (
-    <>
-      <div className='flex flex-col items-center justify-center m-4 sm:m-8'>
-        <Introduce />
-        <div className='flex container flex-wrap w-fit m-4'>
-          <Tags posts={posts} />
-        </div>
-        <div className='flex flex-col container'>
-          <PostInfos posts={posts}/>
-        </div>
+    <div className='flex flex-col items-center justify-center m-4 sm:m-8'>
+      <Introduce />
+      <div className='flex flex-col container mt-10'>
+        <PostInfos posts={posts}/>
       </div>
-    </>
+    </div>
   )
 }
 
