1131f5fffe6de40d0a35fffddaad4db7355c730d
soodev20 <soodev20@gmail.com>
2022-11-25 23:27:35 +0900

    Feat: Tag system, Tags page

--- pages/tags.tsx
+++ pages/tags.tsx
@@ -1,9 +1,27 @@
-const TagsPage = () => {
+import { GetStaticProps } from 'next'
+import Tags from '../components/Tags'
+import { getAllPostsMetaData } from './api/getStaticData'
+import { iPostsType } from '../type/type'
+
+const TagsPage = ({ posts }: iPostsType): JSX.Element => {
   return (
-    <div className='flex flex-col items-center justify-center m-10'>
-      <p>This is tags page.</p>
+    <div className='flex flex-row items-center justify-center m-10'>
+      <div className='flex items-center justify-center m-8'>
+        <span className='text-xl sm:text-2xl'>Tags</span>
+      </div>
+      <div className='flex container flex-wrap w-fit m-4'>
+        <Tags posts={posts} />
+      </div>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPostsMetaData()
+
+  return {
+    props: { posts },
+  }
 }
 
 export default TagsPage
