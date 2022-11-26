370bab0717e491cff5a9536c85af71052a0ca546
soodev20 <soodev20@gmail.com>
2022-11-26 16:11:14 +0900

    Add: Projects examples

---
+++ data/common/projectInfo.ts
@@ -0,0 +1,10 @@
+import { iProjectType } from "../../type/type"
+
+export const PROJECT_LIST: Array<iProjectType> = [
+  {
+    title: 'devlog',
+    description: '블로그 프로젝트!',
+    url: 'https://github.com/Soodev20/devlog',
+    imgUrl: '/img/example.png'
+  }
+]
