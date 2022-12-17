e10363248f507279ec0f2aaf3cd19fa273549a8e
Soodev20 <soodev20@gmail.com>
2022-12-17 17:47:17 +0900

    Chore: Add projectInfo example

--- data/common/projectInfo.ts
+++ data/common/projectInfo.ts
@@ -5,6 +5,18 @@ export const PROJECT_LIST: Array<iProjectType> = [
     title: 'devlog',
     description: '블로그 프로젝트!',
     url: 'https://github.com/Soodev20/devlog',
-    imgUrl: '/img/example.png'
+    imgUrl: '/data/img/example.png'
+  },
+  {
+    title: 'devlog',
+    description: '블로그 프로젝트!',
+    url: 'https://github.com/Soodev20/devlog',
+    imgUrl: '/data/img/example.png'
+  },
+  {
+    title: 'devlog',
+    description: '블로그 프로젝트!',
+    url: 'https://github.com/Soodev20/devlog',
+    imgUrl: '/data/img/example.png'
   }
 ]
